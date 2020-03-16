7fd801bd2a2e8a754a2750996a65811bac152643
Elana Ryznar <eryzna@gmail.com>
2020-03-16 04:04:19 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -45,8 +45,19 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
-  end
+      if params[:artist_id]
+        artist = Artist.find_by(id: params[:artist_id])
+        if artist.nil?
+          redirect_to artists_path, alert: "Artist not found"
+        else
+          @song = artist.songs.find_by(id: params[:id])
+          redirect_to artist_songs_path(artist), alert: "Song not found" if @song.nil?
+        end
+      else
+        @song = Song.find(params[:id])
+      end
+    end
+
 
   def update
     @song = Song.find(params[:id])
