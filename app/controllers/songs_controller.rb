cb41fe305b39538d492698eba547b71cde84c79b
Elana Ryznar <eryzna@gmail.com>
2020-03-16 03:59:43 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,12 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new(artist_id: params[:artist_id])
+    if params[:artist_id] && !Artist.exists?
+      params[:artist_id]
+      redirect to artists_path
+    else
+      @song= Song.new(artist_id: params[:artist_id])
+    end
   end
 
   def create
